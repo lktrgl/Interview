0a684fc569a1f642bf96926ea8422258c314190b
lktrgl <32882989+lktrgl@users.noreply.github.com>
2020-05-18 16:47:38 +0300

    really initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "mega_c_include.h"
+
+int main()
+{
+  printf ( "%s\n", "BOO" );
+
+  return 0;
+}
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "mega_cpp_include.h"
+
+int main()
+{
+  std::cout << "BOO" << std::endl;
+
+  return 0;
+}
+
