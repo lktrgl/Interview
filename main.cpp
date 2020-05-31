ad1cf910c1f24a66972a259497a50df7052661c9
lktrgl <32882989+lktrgl@users.noreply.github.com>
2020-05-31 20:23:09 +0300

    added some useful peaces of the code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include "mega_cpp_include.h"
 
+//----------------------------------
+// use the dummy call finding out the actual type
+// catchType<>();
+// catchType<decltype()>();
+
+template<typename T>
+void catchType();
+
+//----------------------------------
+// include the namespaces for convenience
+
+using namespace std;
+using namespace std::chrono;
+
+//----------------------------------
+// Put your code below :
+
+
+
+
 int main()
 {
   std::cout << "BOO" << std::endl;
